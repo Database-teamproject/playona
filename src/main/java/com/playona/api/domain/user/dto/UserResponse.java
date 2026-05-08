7ab582d25d9b52730736186ff64b4da60c12266e
ThuMyatHtoo29 <153301599+ThuMyatHtoo29@users.noreply.github.com>
2026-05-08 18:48:56 +0900

    Update UserResponse.java
    Add JsonPropertyOrder annotation to UserResponse class.

--- src/main/java/com/playona/api/domain/user/dto/UserResponse.java
+++ src/main/java/com/playona/api/domain/user/dto/UserResponse.java
@@ -1,5 +1,6 @@
 package com.playona.api.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,6 +8,13 @@ import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@JsonPropertyOrder({
+        "userUuid",
+        "email",
+        "nickname",
+        "profileImageUrl",
+        "createdAt"
+})
 public class UserResponse {
     private String userUuid;
     private String email;
